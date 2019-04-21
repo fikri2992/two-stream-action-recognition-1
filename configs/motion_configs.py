bd2aad3ed98c9bb00c61dab26e0877a9c0e1b0f9
fikri nugraha <fikri.nugraha@student.unjani.ac.id>
2019-04-21 14:28:08 +0700

    back to resnet

--- configs/motion_configs.py
+++ configs/motion_configs.py
@@ -19,15 +19,15 @@ this includes: pretrained and from scratch resnet/xception/vgg19/mobile net hype
 # augmenter_level = 1  # 0 heavy , 1 medium,2 simple
 ###############################################################################
 """ medium,sgd,pretrained,5e-5,resnet  """
-# is_adam = False
-# pretrained = True
-# testing_samples_per_video = 19
-# lr = 5e-5
-# model_name = "resnet"  # resnet xception vgg mobilenet
-# epochs = 100
-# validate_every = 5
-# stacked_frames = 10
-# augmenter_level = 1  # 0 heavy , 1 medium,2 simple
+is_adam = False
+pretrained = True
+testing_samples_per_video = 19
+lr = 5e-5
+model_name = "resnet"  # resnet xception vgg mobilenet
+epochs = 100
+validate_every = 5
+stacked_frames = 10
+augmenter_level = 1  # 0 heavy , 1 medium,2 simple
 ###############################################################################
 """ medium,adam,scratch,5e-5,resnet  """
 # is_adam = True
@@ -74,15 +74,15 @@ this includes: pretrained and from scratch resnet/xception/vgg19/mobile net hype
 # augmenter_level = 1  # 0 heavy , 1 medium,2 simple
 ###############################################################################
 """ heavy,adam,pretrained,10e-6,xception"""
-is_adam = True
-pretrained = True
-testing_samples_per_video = 19
-lr = 10e-6
-model_name = "xception"  # resnet xception vgg mobilenet
-epochs = 10
-validate_every = 5
-stacked_frames = 10
-augmenter_level = 0  # 0 heavy , 1 medium,2 simple
+# is_adam = True
+# pretrained = True
+# testing_samples_per_video = 19
+# lr = 5e-6
+# model_name = "xception"  # resnet xception vgg mobilenet
+# epochs = 10
+# validate_every = 5
+# stacked_frames = 10
+# augmenter_level = 0  # 0 heavy , 1 medium,2 simple
 ###############################################################################
 """ medium,sgd,pretrained,5e-6,xception"""
 # is_adam = False
--- motion_trainer.py
+++ motion_trainer.py
@@ -114,7 +114,7 @@ else:
     keras_motion_model.fit_generator(train_loader.get_sustaining_generator(),
                                      steps_per_epoch=len(train_loader),  # generates a batch per step
                                      epochs=epochs,
-                                     use_multiprocessing=True, workers=workers,
+                                     use_multiprocessing=False, workers=workers,
                                      # validation_data=gen_test(), validation_steps=len(test_loader.dataset)
                                      callbacks=[MotionValidationCallback(model=keras_motion_model, test_loader=test_loader, test_video_level_label=test_video_level_label),  # returns callback instance
                                                 keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=validate_every * 10, verbose=1)],
