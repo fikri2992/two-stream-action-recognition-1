25b40f04c4f32d028eadc2e2e6f078c83d7021cd
fikri nugraha <fikri.nugraha@student.unjani.ac.id>
2019-05-11 15:14:14 +0700

    ganti lagi konfigurasi

--- configs/spatial_configs.py
+++ configs/spatial_configs.py
@@ -73,7 +73,7 @@ pretrained = True
 testing_samples_per_video = 19
 lr = 10e-6
 model_name = "xception"  # resnet xception vgg mobilenet
-epochs = 100
+epochs = 10
 validate_every = 5
 augmenter_level = 2  # 0 heavy , 1 medium,2 simple
 ###############################################################################
