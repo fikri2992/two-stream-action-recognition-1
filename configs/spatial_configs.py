b94cfdcf2fa5ba0d82810da645f405c22b51d5e8
fikri nugraha <fikri.nugraha@student.unjani.ac.id>
2019-05-14 03:43:36 +0700

    resnet

--- configs/spatial_configs.py
+++ configs/spatial_configs.py
@@ -38,11 +38,11 @@ this includes: pretrained and from scratch resnet/xception/vgg19/mobile net hype
 # augmenter_level = 1  # 0 heavy , 1 medium,2 simple
 ###############################################################################
 """ medium,adam,pretrained,5e-5,xception 86.12%"""
-is_adam = False
+is_adam = True
 pretrained = True
 testing_samples_per_video = 19
 lr = 5e-5
-model_name = "vgg"  # resnet xception vgg mobilenet
+model_name = "resnet"  # resnet xception vgg mobilenet
 epochs = 100
 validate_every = 5
 augmenter_level = 1  # 0 heavy , 1 medium,2 simple
